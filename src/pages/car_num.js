734a178566b02317edd9714a7d13fd4042d12e6e
王军伟 <wangjunwei@yiche.com>
2021-01-15 17:12:35 +0800

    车牌输入框

--- src/pages/car_num.js
+++ src/pages/car_num.js
@@ -1,10 +1,27 @@
 import React, { Component }from 'react';
 import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 
+import AddVehicle from "../component/vehicleKeyBord/AddVehicle";
+
 class CarNumKeyboard extends Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      modelVisible: false
+    }
+  }
 
   _onPress = () => {
+    this.setState({
+      modelVisible: true
+    })
+  }
 
+  _closeModel = () => {
+    this.setState({
+      modelVisible: false
+    })
   }
 
   render() {
@@ -12,6 +29,11 @@ class CarNumKeyboard extends Component{
       <TouchableOpacity onPress={this._onPress}>
         <Text>显示键盘</Text>
       </TouchableOpacity>
+
+      <AddVehicle
+        modalVisible={this.state.modelVisible}
+        closeModal={this._closeModel}
+      />
     </View>;
   }
 }
